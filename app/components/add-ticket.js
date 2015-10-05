14918d1d3eafb8d80acb445ea6cd3b501f1f275d
Summer Brochtrup & Ashley Sullins <student@epicodus.com>
2015-10-05 11:31:23 -0700

    Add radio buttons to select class for ticket

--- app/components/add-ticket.js
+++ app/components/add-ticket.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
       var params = {
         names: this.get('names') ? this.get('names') : "",
         question: this.get('question') ? this.get('question') : "",
+        class: this.get('class') ? this.get('class') : "",
         date_added: Date.now()
       };
       this.sendAction('saveTicket', params);
--- app/models/ticket.js
+++ app/models/ticket.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   names: DS.attr(),
   question: DS.attr(),
+  class: DS.attr(),
   date_added: DS.attr()
 });
